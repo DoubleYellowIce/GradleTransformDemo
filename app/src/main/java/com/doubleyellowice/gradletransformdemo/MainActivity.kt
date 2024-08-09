9267edac6ac89fc1f8c75db1b2594fbacf5bab72
doubleyellowice <doubleyellowice@gmail.com>
2024-08-09 09:14:03 +0800

    first commit

---
+++ app/src/main/java/com/doubleyellowice/gradletransformdemo/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.doubleyellowice.gradletransformdemo
+
+import android.app.Activity
+import android.os.Bundle
+
+
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
